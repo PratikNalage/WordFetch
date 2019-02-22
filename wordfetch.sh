9c919c3945d56a5ce4fb6b65273cc29a4e27ed81
Pratik <pknalage@gmail.com>
2019-02-23 02:13:26 +0530

    Renamed main.sh to wordfetch.sh

--- wordfetch.sh
+++ wordfetch.sh
@@ -1,4 +1,4 @@
 rand=$((RANDOM%1163))
-python main.py $rand 0 | figlet -k
+python main.py $rand 0 | figlet -k | lolcat
 echo "Word :" $(python main.py $rand 0)
 echo "Meaning :" $(python main.py $rand 1)
